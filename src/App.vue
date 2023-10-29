1bff9ef07c432f317e3e178ff210055e85d7077e
owwwwwo03 <be0ambitious@gmail.com>
2023-10-29 12:43:48 +0800

    first version

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Home></Home>
+  </div>  
+</template>
+
+<script lang="ts">
+  import Home from './views/Home.vue';
+
+</script>
+
+<style scoped>
+[v-cloak]{
+  display: none;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>你好</h1>
+</template>
+<script lang="ts">
+
+</script>
+export default {
+    components: {
+      Home,
+    },
+  }
+<style scoped>
+
+</style>
