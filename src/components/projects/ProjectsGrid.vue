b0204adc34accc58615c377729d2c65d41db907f
SeanYXSu <be0ambitious@gmail.com>
2023-11-07 16:36:05 +0800

    20231107-調整框架

---
+++ src/components/projects/ProjectsGrid.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- Projects grid -->
+   <section class="pt-10 sm:pt-14">
+        <!-- Projects grid title -->
+
+        <!-- Filter and search projects -->
+   </section>
+
+</template>
+
+<script lang="ts" setup>
+    
+</script>
+
+<style>
+
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,9 +1,11 @@
 <template>
-    
+    <div class="container mx-auto">
+		<ProjectsGrid />
+	</div>
 </template>
 
 <script lang="ts">
-    
+    import ProjectsGrid from '../components/projects/ProjectsGrid.vue';
 </script>
 
 <style>
