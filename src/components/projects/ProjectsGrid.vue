7edb0de9a4ef4cd4a26d52f61b49cc52c02aa57d
SeanYXSu <be0ambitious@gmail.com>
2023-11-08 16:59:43 +0800

    20231108-調整框架

--- src/components/projects/ProjectsGrid.vue
+++ src/components/projects/ProjectsGrid.vue
@@ -1,10 +1,42 @@
 <template>
     <!-- Projects grid -->
-   <section class="pt-10 pt-sm-14">
-        <!-- Projects grid title -->
+    <section class="pt-5 pt-sm-5">
+    <!-- Projects grid title -->
+    <div class="text-center">
+        <h2 class="font-weight-bold text-primary">Projects</h2>
+    </div>
 
-        <!-- Filter and search projects -->
-   </section>
+    <!-- Filter and search projects -->
+    <div class="mt-5 mt-sm-5">
+        <h3 class="font-weight-normal text-center text-secondary mb-4">
+            Search by title or filter by category
+        </h3>
+        <div class="d-flex justify-content-between border-bottom border-primary pb-3 mb-2">
+            <div class="d-flex align-items-center">
+                <span class="bg-primary p-2.5 shadow rounded-circle cursor-pointer">
+                    <i class="fas fa-search text-secondary"></i>
+                </span>
+                <input
+                    class="form-control pl-3 pr-1 py-2 border border-secondary rounded text-secondary bg-light"
+                    id="name"
+                    name="name"
+                    type="search"
+                    placeholder="Search project"
+                    aria-label="Name"
+                />
+            </div>
+            
+        </div>
+    </div>
+
+    <!-- Projects grid -->
+    <div class="row mt-4">
+        <div class="col-12 col-sm-6 col-lg-4">
+            
+        </div>
+    </div>
+</section>
+
 
 </template>
 
--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,26 +1,64 @@
 <template>
-  <div class="container mx-auto ">
-    <div
-			class="pt-5 pb-2 mt-5 border-top border-primary-light dark:border-secondary-dark"
-		>
+  <div class="container">
+    <div class="pt-5 pb-3 border-top border-primary-light">
         <!-- Footer social links -->
-        <div
-				class="d-flex flex-column justify-content-center align-items-center mb-3 mb-sm-5"
-			>
-				<p
-					class="font-weight-bold fs-3 fs-sm-4 text-primary-dark dark:text-primary-light mb-2"
-				>
-					Follow me
-				</p>
+        <div class="row justify-content-center mb-4">
+            <div class="col-12">
+                <h3 class="font-weight-bold text-primary-dark">
+                    Follow-me
+                </h3>
+            </div>
+            <div class="col-12">
+                <ul class="list-inline">
+					<h1>ooo</h1>
+                    <li v-for="social in socials" :key="social.id" class="list-inline-item">
+                        <a :href="social.url" target="_blank" class="text-gray-400">
+                            <i :class="social.icon"></i>
+							<i :class="social.name"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <!-- Footer copyright -->
 
+        <!-- Footer copyright -->
+        
     </div>
-  </div>
+</div>
 </template>
 
 <script lang="ts">
-	
+import feather from 'feather-icons';
+import 'bootstrap/dist/css/bootstrap.css';
+import { onMounted, onUpdated } from 'vue';
+
+const socials =[
+	{
+          id: 2,
+          name: 'Twitter',
+          icon: 'twitter',
+          url: 'https://twitter.com/anappdesign',
+        },
+        {
+          id: 3,
+          name: 'Medium',
+          icon: 'book',
+          url: 'https://stoman.medium.com/anappdesign',
+        },
+        {
+          id: 4,
+          name: 'Instagram',
+          icon: 'instagram',
+          url: 'https://instagram.com/anappdesign',
+        },	
+	];
+
+onMounted(()=>{
+	feather.replace();
+});
+onUpdated(()=>{
+	feather.replace();
+});
 </script>
 
 <style>
