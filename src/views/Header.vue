7a49551a73070280de7ed6f20abfab065db4b97c
SeanYXSu <be0ambitious@gmail.com>
2023-11-13 11:31:16 +0800

    20231113-調整框架

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,28 +1,16 @@
 <template>
-  <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="main-header navbar navbar-expand">
     <!-- Header start -->
-    <div class="container-fluid">
+    <div class="navbar-nav ml-auto">
         <!-- Header menu links and small screen hamburger menu -->
-        <div class="">
               <!-- Header logos -->
-          <div>
-            
-            <a href="/" class="navbar-brand custom-font">Personal</a>
-            
+          <div class="">           
+            <a href="/" class="navbar-brand custom-font">Personal</a>           
           </div>
-              <!-- Theme switcher small screen -->
-              <div>
-                
-              </div>
-              <!-- Small screen hamburger menu -->
-              <div>
-                  
-              </div>
-
-        </div>
-        <!-- Header links -->
-        <HeaderLinks :showModal="showModal" :isOpen="isOpen" />
-        <!-- Header right section buttons -->
+          <!-- Header links -->
+          <div class=""> 
+            <HeaderLinks :showModal="showModal" :isOpen="isOpen" /> 
+          </div>     
     </div>
   </nav>
 </template>
@@ -33,7 +21,18 @@
     const store = useStore();
 </script>
 
-<style>
+<style scoped>
+
+  .main-header{
+    background-color:#ffffff !important;
+    color: #4b545c !important;
+    border: 0px !important;
+  }
+  .navbar-nav{
+    display: flex;
+    gap:4px;
+    margin-right: 1rem;
+  }
   .custom-font{
     font-family: Arial, Helvetica, sans-serif;
   }
