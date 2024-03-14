776d61bbbc167aea6812e219540e9d0146680192
Sean Su <84563542+owwwwwo03@users.noreply.github.com>
2024-03-14 21:30:41 +0800

    vbike content

--- src/components/projects/contents/VBike.vue
+++ src/components/projects/contents/VBike.vue
@@ -2,7 +2,12 @@
     <div :style="{ 'background-image': 'url(' + imageUrl + ')' }" class="background-container">
         <!-- Text on the background image -->
         <div class="text-overlay custom-font">
-            <p>V-Bike</p>
+            <p class="text-overlay-title mb-2">V-Bike</p>
+            <p class="text-overlay-subtitle mb-10">真實全景影片式虛擬實境結合下肢踩踏裝置系統</p>
+            <p class="text-overlay-content">
+                V-Bike 是一套透過生理數據、訓練課程等設計，並結合360度真實全景影片式虛擬實境技術的踩踏裝置系統。
+                透過簡單的操作模式，高齡長輩即使人在病房、室內場所，也能V-Bike前往不同的風景名勝、國外景點，
+                一邊踩踏一邊欣賞當地360度的景色，讓訓練不單單只是訓練，而是一趟美好的旅程。</p>
         </div>
     </div>
 </template>
@@ -32,12 +37,24 @@ const imageUrl = ref(background_vbike);
     overflow: hidden;
     /* Use flexbox to center the text */
 }
-.text-overlay {
+.text-overlay { 
     /* Optional: Add some padding to the text */
-    padding: 160px;
+    padding: 120px;
     color: white; /* Adjust the color as needed */
-    /* Make the font size responsive to the screen size */
+    
+}
+.text-overlay-title { 
     font-size: 10em; /* Adjust the formula as needed */
+    text-align: center; /* Center the text horizontally */
+}
+
+.text-overlay-subtitle { 
+    font-size: 2em; /* Adjust the formula as needed */
+    text-align: center; /* Center the text horizontally */
+}
+
+.text-overlay-content { 
+    font-size: 1em; /* Adjust the formula as needed */
     text-align: center; /* Center the text horizontally */
 }
 
